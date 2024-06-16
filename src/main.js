bb72c2e228c7a93e426abc87c492cfba2d9f52da
Flibouche <kevin.pfiffer2@gmail.com>
2024-06-16 15:15:29 +0200

    Creating the player.

--- src/main.js
+++ src/main.js
@@ -23,6 +23,24 @@ k.scene("main", async () => {
   const layers = mapData.layers;
 
   const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({
+        shape: new k.Rect(k.vec2(0, 3), 10, 10),
+    }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+        // Quand le player est dans une boite de dialogue, il ne peut pas bouger
+        speed: 250,
+        direction: "down",
+        isInDialogue: false, 
+    },
+    "player",
+    ]);
 });
 
 k.go("main");
