5eb333c89add794ea431fdaed92ec50ca16aca86
Flibouche <kevin.pfiffer2@gmail.com>
2024-06-23 15:48:45 +0200

    Add dialogueData to make experimentation.

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,6 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     keyboard: `Hello World`,
-    micro: `Hello world 2`
+    micro: `Hello world 2`,
+    drum: `I used to be a drummer in my teenage years !`
 }
